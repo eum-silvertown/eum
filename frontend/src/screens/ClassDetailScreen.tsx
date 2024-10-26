727758cdcefffa9789baa9518fdbb5384abe7c60
HyoJin <cron.phj@gmail.com>
2024-10-26 19:56:31 +0900

    feat : 세부영역 분할

--- frontend/src/screens/ClassDetailScreen.tsx
+++ frontend/src/screens/ClassDetailScreen.tsx
@@ -5,24 +5,42 @@ import { StyleSheet, View } from 'react-native';
 function ClassDetailScreen(): React.JSX.Element {
     return (
         <View style={styles.container}>
+            {/* 헤더 */}
             <View style={styles.header}>
                 <Text variant="title" weight="bold">
                     수업 상세 정보
                 </Text>
             </View>
+            {/* 본문 */}
             <View style={styles.content}>
                 <View style={styles.row}>
-                    <View style={styles.overview}>
-                        <Text>Overview</Text>
+                    <View style={styles.overviewLayout}>
+                        {/* 오버뷰 */}
+                        <View style={styles.overview}>
+                            <Text>Overview 1</Text>
+                        </View>
+                        {/* 공지사항 */}
+                        <View style={styles.overview}>
+                            <Text>Overview 2</Text>
+                        </View>
                     </View>
-                    <View style={styles.mainContent}>
-                        <Text>Content</Text>
+                    <View style={styles.mainContentLayout}>
+                        {/* 선생님 Detail */}
+                        <View style={styles.mainContent}>
+                            <Text>Content 1</Text>
+                        </View>
+                        {/* 성취도 차트 */}
+                        <View style={styles.mainContent}>
+                            <Text>Content 2</Text>
+                        </View>
                     </View>
                 </View>
                 <View style={styles.row}>
+                    {/* 다시보기 */}
                     <View style={styles.review}>
                         <Text>Review</Text>
                     </View>
+                    {/* 숙제 */}
                     <View style={styles.homework}>
                         <Text>Homework</Text>
                     </View>
@@ -56,15 +74,25 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         gap: spacing.md,
     },
+    overviewLayout: {
+        flex: 4,
+        flexDirection: 'column',
+        gap: spacing.md,
+    },
     overview: {
-        flex: 2,
-        backgroundColor: 'gray',
+        flex: 1,
+        backgroundColor: 'lightgray',
         justifyContent: 'center',
         alignItems: 'center',
     },
+    mainContentLayout: {
+        flex: 2,
+        flexDirection: 'column',
+        gap: spacing.md,
+    },
     mainContent: {
         flex: 1,
-        backgroundColor: 'green',
+        backgroundColor: 'lightgreen',
         justifyContent: 'center',
         alignItems: 'center',
     },
