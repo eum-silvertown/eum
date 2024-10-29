209597b9a56b05a00139324269377e2b84aa7ff9
박효진 <jam_ty@naver.com>
2024-10-29 13:41:22 +0900

    feat : post-it 이미지 추가 및 공지사항 영역 추가

--- frontend/src/components/classDetail/Notice.tsx
+++ frontend/src/components/classDetail/Notice.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Text} from '@components/common/Text';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
+import postit from '@assets/images/postit.png';
+import {getResponsiveSize} from '@utils/responsive';
+import {spacing} from '@theme/spacing';
 
 function Notice(): React.JSX.Element {
   return (
     <View style={styles.notice}>
       <Text>Notice</Text>
+      <View style={styles.noticeLayout}>
+        <Image source={postit} alt="postit" style={styles.imageContainer} />
+        <Image source={postit} alt="postit" style={styles.imageContainer} />
+        <Image source={postit} alt="postit" style={styles.imageContainer} />
+      </View>
     </View>
   );
 }
@@ -14,7 +22,15 @@ const styles = StyleSheet.create({
   notice: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  noticeLayout: {
+    flexDirection: 'row',
+    gap: spacing.xxl,
+    marginHorizontal: 'auto',
+  },
+  imageContainer: {
+    width: getResponsiveSize(168),
+    height: getResponsiveSize(140),
   },
 });
 
