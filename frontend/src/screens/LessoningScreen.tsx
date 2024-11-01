f6788c2a0afd1ee486ec59912f70c0afc7c7ce7e
박효진 <jam_ty@naver.com>
2024-11-01 17:50:36 +0900

    test : Drawing socket 테스트

--- frontend/src/screens/LessoningScreen.tsx
+++ frontend/src/screens/LessoningScreen.tsx
@@ -1,49 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {io, Socket} from 'socket.io-client';
 
 import ProblemSection from '@components/classLessoning/ProblemSection';
 import LeftCanvasSection from '@components/classLessoning/LeftCanvasSection';
 // import LeftRecordCanvasSection from '@components/classLessoning/LeftRecordCanvasSection';
-// import RightCanvasSection from '@components/classLessoning/RightCanvasSection';
-import RightRecordCanvasSection from '@components/classLessoning/RightRecordCanvasSection';
+import RightCanvasSection from '@components/classLessoning/RightCanvasSection';
+// import RightRecordCanvasSection from '@components/classLessoning/RightRecordCanvasSection';
 
 type PathData = {
   path: any;
   color: string;
   strokeWidth: number;
   opacity: number;
-  timestamp: number; // timestamp 포함
+  timestamp: number;
 };
 
 function LessoningScreen(): React.JSX.Element {
+  // const [recordedPaths, setRecordedPaths] = useState<PathData[]>([]);
+
   const socket: Socket = io('http://192.168.128.246:8080', {
-    reconnection: false,
+    reconnection: true,
     secure: true,
     transports: ['websocket'],
   });
-  socket.on('connect_error', err => {
-    console.log(err.message);
-  });
-  const [recordedPaths, setRecordedPaths] = useState<PathData[]>([]); // 녹화된 경로 데이터 저장
 
   const handleRecordingEnd = (paths: PathData[]) => {
-    // path가 Skia Path인 경우, SVG string으로 변환
     const formattedPaths = paths.map(pathData => ({
       ...pathData,
-      path:
-        typeof pathData.path === 'string'
-          ? pathData.path
-          : pathData.path.toSVGString(),
+      path: pathData.path.toSVGString
+        ? pathData.path.toSVGString()
+        : pathData.path,
     }));
-    setRecordedPaths(formattedPaths); // 변환된 데이터를 저장
+    // setRecordedPaths(formattedPaths);
     console.log('중간단계 데이터 확인용', formattedPaths);
   };
 
   useEffect(() => {
     socket.on('connect_error', err => console.log('연결 오류:', err.message));
 
-    // 컴포넌트가 언마운트될 때 소켓 연결 해제
     return () => {
       socket.disconnect();
     };
@@ -51,7 +46,6 @@ function LessoningScreen(): React.JSX.Element {
 
   return (
     <View style={styles.container}>
-      {/* 왼쪽 문제와 캔버스 */}
       <View style={styles.sectionContainer}>
         <ProblemSection />
         <LeftCanvasSection
@@ -61,11 +55,10 @@ function LessoningScreen(): React.JSX.Element {
         {/* <LeftRecordCanvasSection onRecordingEnd={handleRecordingEnd} /> */}
       </View>
 
-      {/* 오른쪽 문제와 캔버스 */}
       <View style={styles.sectionContainer}>
         <ProblemSection />
-        {/* <RightCanvasSection socket={socket} /> */}
-        <RightRecordCanvasSection recordedPaths={recordedPaths} />
+        <RightCanvasSection socket={socket} />
+        {/* <RightRecordCanvasSection recordedPaths={recordedPaths} /> */}
       </View>
     </View>
   );
