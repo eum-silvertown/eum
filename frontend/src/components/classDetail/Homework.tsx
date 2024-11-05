7a4775849fc773d07eb01f87e1b68752e93547d6
박효진 <jam_ty@naver.com>
2024-11-06 01:38:30 +0900

    remove : 숙제 생성 로직 삭제(이동 예정)

--- frontend/src/components/classDetail/Homework.tsx
+++ frontend/src/components/classDetail/Homework.tsx
@@ -1,22 +1,17 @@
-import React, {useState} from 'react';
-import {View, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
-import {Text} from '@components/common/Text';
-import {spacing} from '@theme/spacing';
-import {iconSize} from '@theme/iconSize';
-import AddCircleIcon from '@assets/icons/addCircleIcon.svg';
-import HomeworkCreateModal from './HomeworkCreateModal';
-import {useModal} from 'src/hooks/useModal';
+import React, { useState } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { Text } from '@components/common/Text';
+import { spacing } from '@theme/spacing';
 
 function Homework(): React.JSX.Element {
-  const {open} = useModal();
   const [homeworkData] = useState([
-    {id: '5', title: '숙제 1', dueDate: '11-01', questionCount: 5},
-    {id: '4', title: '숙제 2', dueDate: '11-02', questionCount: 10},
-    {id: '3', title: '숙제 3', dueDate: '11-03', questionCount: 7},
-    {id: '2', title: '숙제 4', dueDate: '11-04', questionCount: 8},
+    { id: '5', title: '숙제 1', dueDate: '11-01', questionCount: 5 },
+    { id: '4', title: '숙제 2', dueDate: '11-02', questionCount: 10 },
+    { id: '3', title: '숙제 3', dueDate: '11-03', questionCount: 7 },
+    { id: '2', title: '숙제 4', dueDate: '11-04', questionCount: 8 },
   ]);
 
-  const renderItem = ({item}: {item: (typeof homeworkData)[0]}) => (
+  const renderItem = ({ item }: { item: (typeof homeworkData)[0] }) => (
     <View style={styles.item}>
       <View style={[styles.textContainer, styles.idContainer]}>
         <Text variant="caption" weight="bold">
@@ -47,17 +42,6 @@ function Homework(): React.JSX.Element {
         <Text variant="subtitle" weight="bold">
           숙제
         </Text>
-        <TouchableOpacity
-          onPress={() => {
-            open(<HomeworkCreateModal />, {
-              title: '숙제 생성',
-              onClose: () => {
-                console.log('숙제 생성 닫기');
-              },
-            });
-          }}>
-          <AddCircleIcon width={iconSize.mdPlus} height={iconSize.mdPlus} />
-        </TouchableOpacity>
       </View>
       <FlatList
         data={homeworkData}
