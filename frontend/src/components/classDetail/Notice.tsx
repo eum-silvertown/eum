a2597d0bb6b77dfc0f6b6fc54cbda3ef44cb85b2
박효진 <jam_ty@naver.com>
2024-11-06 01:18:41 +0900

    feat : Notice 선생님용 생성, 삭제 버튼 추가

--- frontend/src/components/classDetail/Notice.tsx
+++ frontend/src/components/classDetail/Notice.tsx
@@ -1,49 +1,72 @@
 import React from 'react';
-import {Text} from '@components/common/Text';
-import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { Text } from '@components/common/Text';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import postit from '@assets/images/postit.png';
-import {spacing} from '@theme/spacing';
-import {getResponsiveSize} from '@utils/responsive';
+import { spacing } from '@theme/spacing';
+import { getResponsiveSize } from '@utils/responsive';
 import AddCircleIcon from '@assets/icons/addCircleIcon.svg';
-import {iconSize} from '@theme/iconSize';
-import {useModal} from 'src/hooks/useModal';
+import { iconSize } from '@theme/iconSize';
+import { useModal } from 'src/hooks/useModal';
 import NoticeCreateModal from './NoticeCreateModal';
+import CancelIcon from '@assets/icons/cancelIcon.svg';
 
-function Notice(): React.JSX.Element {
-  const {open} = useModal();
+type IsTeacherProps = {
+  isTeacher: boolean;
+}
+
+function Notice({ isTeacher }: IsTeacherProps): React.JSX.Element {
+  const { open } = useModal();
+
+  const noticeDatas = [
+    {
+      title: '공지사항 1',
+      content: '공지사항 내용 1@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
+    },
+    {
+      title: '공지사항 2',
+      content: '공지사항 내용 2',
+    },
+    {
+      title: '공지사항 3',
+      content: '공지사항 내용 3',
+    },
+  ];
+
   return (
     <View style={styles.notice}>
       <View style={styles.noticeHeader}>
         <Text variant="subtitle" weight="bold">
           공지사항
         </Text>
-        <TouchableOpacity
-          onPress={() => {
-            open(<NoticeCreateModal />, {
-              title: '공지사항 생성',
-              onClose: () => {
-                console.log('공지사항 생성 닫기');
-              },
-            });
-          }}>
-          <AddCircleIcon width={iconSize.mdPlus} height={iconSize.mdPlus} />
-        </TouchableOpacity>
+        {isTeacher && (
+          <TouchableOpacity
+            onPress={() => {
+              open(<NoticeCreateModal />, {
+                title: '공지사항 생성',
+                onClose: () => {
+                  console.log('공지사항 생성 닫기');
+                },
+              });
+            }}>
+            <AddCircleIcon width={iconSize.md} height={iconSize.md} />
+          </TouchableOpacity>
+        )}
       </View>
       <View style={styles.noticeLayout}>
-        <View style={styles.imageWrapper}>
-          <Image source={postit} alt="postit" style={styles.imageContainer} />
-          <Text style={styles.overlayText}>공지사항 1</Text>
-        </View>
-
-        <View style={styles.imageWrapper}>
-          <Image source={postit} alt="postit" style={styles.imageContainer} />
-          <Text style={styles.overlayText}>공지사항 2</Text>
-        </View>
-
-        <View style={styles.imageWrapper}>
-          <Image source={postit} alt="postit" style={styles.imageContainer} />
-          <Text style={styles.overlayText}>공지사항 3</Text>
-        </View>
+        {noticeDatas.map((notice, index) => (
+          <View key={index} style={styles.imageWrapper}>
+            <Image source={postit} alt="postit" style={styles.imageContainer} />
+            <View style={styles.textIconContainer}>
+              <Text style={styles.overlayText}>{notice.title}</Text>
+              {isTeacher &&
+                <TouchableOpacity style={styles.cancelIcon}>
+                  <CancelIcon width={iconSize.xxs} height={iconSize.xxs} />
+                </TouchableOpacity>
+              }
+            </View>
+            <Text style={styles.noticeContent}>{notice.content}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -75,11 +98,26 @@ const styles = StyleSheet.create({
     height: '100%',
     objectFit: 'fill',
   },
-  overlayText: {
+  textIconContainer: {
     position: 'absolute',
-    marginTop: spacing.xl,
-    marginLeft: spacing.lg,
+    left: 12,
+    top: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  overlayText: {
     fontWeight: 'bold',
+  },
+  noticeContent: {
+    position: 'absolute',
+    left: 12,
+    right: 12,
+    top: 42,
+    color: '#333',
+  },
+  cancelIcon: {
+    marginLeft: '46%',
   },
 });
 
