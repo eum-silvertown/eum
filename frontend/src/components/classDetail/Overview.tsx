51a054cbb4953010ac16c27614a9dc2d2b6a76b6
박효진 <jam_ty@naver.com>
2024-11-06 00:48:48 +0900

    feat : 선생님일 경우 IntoArrow를 통해 리스트로 이동할 경로 추가

--- frontend/src/components/classDetail/Overview.tsx
+++ frontend/src/components/classDetail/Overview.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Text} from '@components/common/Text';
-import {View, StyleSheet} from 'react-native';
+import { Text } from '@components/common/Text';
+import { View, StyleSheet } from 'react-native';
 import ProgressBox from '@components/homework/ProgressBox';
-import {spacing} from '@theme/spacing';
-import {getResponsiveSize} from '@utils/responsive';
+import { spacing } from '@theme/spacing';
+import { getResponsiveSize } from '@utils/responsive';
 
-function Overview(): React.JSX.Element {
+type IsTeacherProps = {
+  isTeacher: boolean;
+}
+
+function Overview({ isTeacher }: IsTeacherProps): React.JSX.Element {
   return (
     <View style={styles.overview}>
       <Text variant="subtitle" weight="bold" style={styles.subtitle}>
@@ -17,7 +21,8 @@ function Overview(): React.JSX.Element {
           title="숙제"
           content="10"
           unit="개"
-          icon="complete"
+          icon="homeworkCheck"
+          isTeacher={isTeacher}
         />
         <ProgressBox
           color="red"
@@ -25,13 +30,15 @@ function Overview(): React.JSX.Element {
           content="10"
           unit="번"
           icon="complete"
+          isTeacher={isTeacher}
         />
         <ProgressBox
           color="green"
           title="문제 보관함"
           content="10"
           unit="개"
-          icon="complete"
+          icon="folderCheck"
+          isTeacher={isTeacher}
         />
       </View>
     </View>
