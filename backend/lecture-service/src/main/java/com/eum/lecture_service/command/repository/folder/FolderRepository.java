ae04e5b2f1ff235bc8565f28ef2b101da90a432d
이예찬 <dldpcks34@naver.com>
2024-11-05 16:46:33 +0900

    refactor(#S11P31D101-414): 폴더 관련 로직 삭제

--- backend/lecture-service/src/main/java/com/eum/lecture_service/command/repository/folder/FolderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eum.lecture_service.command.repository.folder;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eum.lecture_service.command.entity.folder.Folder;
-
-public interface FolderRepository extends JpaRepository<Folder, Long> {
-}
