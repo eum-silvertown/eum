8acdd33b50feacf8b8990cfd3e621af63873fe15
이예찬 <dldpcks34@naver.com>
2024-11-05 16:49:05 +0900

    feat(#S11P31D101-416): 수업/숙제/시험 관련 DTO 구현

---
+++ backend/lecture-service/src/main/java/com/eum/lecture_service/command/dto/homework/HomeworkProblemSubmissionDto.java
@@ -0,0 +1,33 @@
+package com.eum.lecture_service.command.dto.homework;
+
+import com.eum.lecture_service.command.entity.homework.HomeworkProblemSubmission;
+import com.eum.lecture_service.command.entity.homework.HomeworkSubmission;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HomeworkProblemSubmissionDto {
+
+	private Long homeworkProblemSubmissionId;
+	private Long homeworkSubmissionId;
+	private Long questionId;
+	private Long studentId;
+	private Boolean isCorrect;
+
+	public HomeworkProblemSubmission toEntity(HomeworkSubmission homeworkSubmission) {
+		return HomeworkProblemSubmission.builder()
+			.homeworkSubmission(homeworkSubmission)
+			.questionId(questionId)
+			.studentId(studentId)
+			.isCorrect(isCorrect)
+			.build();
+	}
+}
---
+++ backend/lecture-service/src/main/java/com/eum/lecture_service/command/dto/lesson/LessonDto.java
@@ -0,0 +1,32 @@
+package com.eum.lecture_service.command.dto.lesson;
+
+import java.util.List;
+
+import com.eum.lecture_service.command.entity.lecture.Lecture;
+import com.eum.lecture_service.command.entity.lesson.Lesson;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LessonDto {
+
+	private Long lessonId;
+	private Long lectureId;
+	private String title;
+	private List<Long> questionIds;
+
+	public Lesson toLessonEntity(Lecture lecture) {
+		return Lesson.builder()
+			.lecture(lecture)
+			.title(title)
+			.build();
+	}
+}
