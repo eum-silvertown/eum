8af3ed0755b8aad36394f70b4f8313140efd090f
박효진 <jam_ty@naver.com>
2024-11-05 17:28:17 +0900

    feat : 숙제 생성 Modal 로직 추가

--- frontend/src/components/classDetail/Homework.tsx
+++ frontend/src/components/classDetail/Homework.tsx
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text} from '@components/common/Text';
 import {spacing} from '@theme/spacing';
+import {iconSize} from '@theme/iconSize';
+import AddCircleIcon from '@assets/icons/addCircleIcon.svg';
+import HomeworkCreateModal from './HomeworkCreateModal';
+import {useModal} from 'src/hooks/useModal';
 
 function Homework(): React.JSX.Element {
+  const {open} = useModal();
   const [homeworkData] = useState([
     {id: '5', title: '숙제 1', dueDate: '11-01', questionCount: 5},
     {id: '4', title: '숙제 2', dueDate: '11-02', questionCount: 10},
@@ -38,9 +43,22 @@ function Homework(): React.JSX.Element {
 
   return (
     <View style={styles.homework}>
-      <Text variant="subtitle" weight="bold" style={styles.subtitle}>
-        숙제
-      </Text>
+      <View style={styles.homeworkHeader}>
+        <Text variant="subtitle" weight="bold">
+          숙제
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            open(<HomeworkCreateModal />, {
+              title: '숙제 생성',
+              onClose: () => {
+                console.log('숙제 생성 닫기');
+              },
+            });
+          }}>
+          <AddCircleIcon width={iconSize.mdPlus} height={iconSize.mdPlus} />
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={homeworkData}
         renderItem={renderItem}
@@ -54,7 +72,9 @@ const styles = StyleSheet.create({
   homework: {
     paddingVertical: spacing.md,
   },
-  subtitle: {
+  homeworkHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
     marginStart: spacing.xl,
   },
   item: {
