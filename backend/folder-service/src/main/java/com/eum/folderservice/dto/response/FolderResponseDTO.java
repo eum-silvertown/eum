5061d0ca0cb593c08b924dcc3c51cd717197dc06
이지언 <dlwldjs7544@naver.com>
2024-11-05 17:34:19 +0900

    refactor(#S11P31D101-312) : 폴더 조회 필드 수정

--- backend/folder-service/src/main/java/com/eum/folderservice/dto/response/FolderResponseDTO.java
+++ backend/folder-service/src/main/java/com/eum/folderservice/dto/response/FolderResponseDTO.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 @Builder
 public class FolderResponseDTO {
     private Long folderId;
+    private Long parentId;
     private String title;
     private Long childrenCount;
 
     public static FolderResponseDTO of(Folder folder) {
         return FolderResponseDTO.builder()
                 .folderId(folder.getId())
+                .parentId(folder.getParentFolder() == null ? null : folder.getParentFolder().getId())
                 .title(folder.getTitle())
                 .childrenCount(folder.getChildrenCount())
                 .build();
--- backend/folder-service/src/main/java/com/eum/folderservice/dto/response/SavedFileResponseDTO.java
+++ backend/folder-service/src/main/java/com/eum/folderservice/dto/response/SavedFileResponseDTO.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @Builder
 public class SavedFileResponseDTO {
     private Long fileId;
+    private Long parentId;
     private String title;
 
     public static SavedFileResponseDTO of(SavedFile savedFile) {
         return SavedFileResponseDTO.builder()
                 .fileId(savedFile.getId())
+                .parentId(savedFile.getFolder().getId())
                 .title(savedFile.getTitle())
                 .build();
     }
