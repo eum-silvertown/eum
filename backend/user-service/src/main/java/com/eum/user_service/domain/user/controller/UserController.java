3582cdde6bfeb3d5622dc65ab335fcfa79702b22
정진규 <wlsrb6905@naver.com>
2024-11-06 17:41:48 +0900

    docs(#S11P31D101-280) : swagger header required false 설정

--- backend/user-service/src/main/java/com/eum/user_service/domain/user/controller/UserController.java
+++ backend/user-service/src/main/java/com/eum/user_service/domain/user/controller/UserController.java
@@ -47,34 +47,34 @@ public class UserController implements UserApiDocumentation{
     }
 
     @GetMapping("/logout")
-    public CommonResponse<?> logout(@RequestHeader(value = "X-MEMBER-ID") String memberId,
-                                    @RequestHeader(value = "Authorization") String token) {
+    public CommonResponse<?> logout(@RequestHeader(value = "X-MEMBER-ID", required = false) String memberId,
+                                    @RequestHeader(value = "Authorization", required = false) String token) {
         userService.logout(Long.valueOf(memberId), token);
         return CommonResponse.success("로그아웃에 성공했습니다.");
     }
 
     @PatchMapping("/info/password")
-    public CommonResponse<?> updateMemberPassword(@RequestHeader(value = "X-MEMBER-ID") String memberId,
+    public CommonResponse<?> updateMemberPassword(@RequestHeader(value = "X-MEMBER-ID", required = false) String memberId,
                                                   @RequestBody PasswordUpdateRequest passwordUpdateRequest) {
         userService.updateMemberPassword(Long.valueOf(memberId), passwordUpdateRequest);
         return CommonResponse.success("비밀번호 변경에 성공했습니다.");
     }
 
     @DeleteMapping("/info")
-    public CommonResponse<?> cancelMember(@RequestHeader(value = "X-MEMBER-ID") String memberId) {
+    public CommonResponse<?> cancelMember(@RequestHeader(value = "X-MEMBER-ID", required = false) String memberId) {
         userService.deleteMemberInfo(Long.valueOf(memberId));
         return CommonResponse.success("성공적으로 탈퇴 되었습니다.");
     }
 
     @GetMapping("/info")
-    public CommonResponse<?> getUserInfo(@RequestHeader(value = "X-MEMBER-ID") String memberId,
-                                         @RequestHeader(value = "X-MEMBER-ROLE") Role role) {
+    public CommonResponse<?> getUserInfo(@RequestHeader(value = "X-MEMBER-ID", required = false) String memberId,
+                                         @RequestHeader(value = "X-MEMBER-ROLE", required = false) Role role) {
         MemberInfoResponse userInfoResponse = userService.getMemberInfo(Long.valueOf(memberId), role);
         return CommonResponse.success(userInfoResponse,"유저 정보를 성공적으로 조회했습니다.");
     }
 
     @PatchMapping("/info/image")
-    public CommonResponse<?> updateMemberProfileImage(@RequestHeader(value = "X-MEMBER-ID") String memberId) {
+    public CommonResponse<?> updateMemberProfileImage(@RequestHeader(value = "X-MEMBER-ID", required = false) String memberId) {
         ImageResponse imageResponse = userService.updateMemberProfile(Long.valueOf(memberId));
         return CommonResponse.success(imageResponse,"프로필 사진이 성공적으로 변경되었습니다.");
     }
