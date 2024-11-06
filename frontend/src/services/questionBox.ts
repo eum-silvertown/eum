335ec42f9081b374fbbc814088bd22b5ba5f9ace
손민락 <dungguk@naver.com>
2024-11-06 14:22:23 +0900

    feat: 문제 보관함 API 연동

--- frontend/src/services/questionBox.ts
+++ frontend/src/services/questionBox.ts
@@ -1,3 +1,4 @@
+import {QuestionBoxType} from '@store/useQuestionExplorerStore';
 import axios from 'axios';
 
 // API 응답 타입 정의
@@ -5,6 +6,69 @@ type ApiResponse<T> = {
   code: string;
   data: T;
   message: string;
+};
+
+type FileType = {
+  fileId: number;
+  parentId: number;
+  title: string;
+};
+
+type FolderType = {
+  folderId: number;
+  parentId: number;
+  title: string;
+  childrenCount: number;
+};
+
+type GetFolderResponse = {
+  folderId: number;
+  subFolders: FolderType[];
+  subFiles: FileType[];
+};
+
+axios.defaults.headers['X-MEMBER-ID'] = 1;
+
+export const getRootFolder = async (): Promise<QuestionBoxType> => {
+  try {
+    const {data} = await axios.get<ApiResponse<GetFolderResponse>>(
+      'folder/root',
+    );
+    console.log(data.data.subFolders);
+    let rootFolder: QuestionBoxType = {
+      id: data.data.folderId,
+      type: 'folder',
+      parentId: null,
+      title: '홈',
+      children: [],
+      childrenCount: 0,
+    };
+    data.data.subFolders.forEach(folder => {
+      const formedFolder: QuestionBoxType = {
+        id: folder.folderId,
+        type: 'folder',
+        parentId: folder.parentId,
+        title: folder.title,
+        children: [],
+        childrenCount: folder.childrenCount,
+      };
+      rootFolder.children?.push(formedFolder);
+    });
+    data.data.subFiles.forEach(file => {
+      const formedFile: QuestionBoxType = {
+        id: file.fileId,
+        type: 'file',
+        parentId: file.parentId,
+        title: file.title,
+        children: [],
+        childrenCount: 0,
+      };
+      rootFolder.children?.push(formedFile);
+    });
+    return rootFolder;
+  } catch (error) {
+    throw error;
+  }
 };
 
 type CreateFolderResponse = {
@@ -16,8 +80,7 @@ type CreateFolderResponse = {
 export const createFolder = async (
   title: string,
   parentId: number,
-): Promise<CreateFolderResponse> => {
-  axios.defaults.headers['X-MEMBER-ID'] = 1;
+): Promise<QuestionBoxType> => {
   try {
     const {data} = await axios.post<ApiResponse<CreateFolderResponse>>(
       'folder',
@@ -26,7 +89,90 @@ export const createFolder = async (
         parentId: parentId,
       },
     );
-    return data.data; // 실제 폴더 데이터만 반환
+
+    return {
+      id: data.data.folderId,
+      type: 'folder',
+      parentId,
+      title,
+      children: [],
+      childrenCount: 0,
+    }; // 실제 폴더 데이터만 반환
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getFolder = async (
+  folderId: number,
+): Promise<QuestionBoxType[]> => {
+  try {
+    const {data} = await axios.get<ApiResponse<GetFolderResponse>>(
+      `folder/${folderId}`,
+    );
+    let children: QuestionBoxType[] = [];
+    data.data.subFolders.forEach(folder => {
+      const formedFolder: QuestionBoxType = {
+        id: folder.folderId,
+        type: 'folder',
+        parentId: folder.parentId,
+        title: folder.title,
+        children: [],
+        childrenCount: folder.childrenCount,
+      };
+      children.push(formedFolder);
+    });
+    data.data.subFiles.forEach(file => {
+      const formedFile: QuestionBoxType = {
+        id: file.fileId,
+        type: 'file',
+        parentId: file.parentId,
+        title: file.title,
+        children: [],
+        childrenCount: 0,
+      };
+      children.push(formedFile);
+    });
+    return children;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteFolder = async (folderId: number) => {
+  try {
+    const {data} = await axios.delete<ApiResponse<null>>(`folder/${folderId}`);
+    console.log(data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const renameFolder = async (
+  folderId: number,
+  title: string,
+): Promise<string> => {
+  try {
+    const {data} = await axios.patch<ApiResponse<CreateFolderResponse>>(
+      'folder',
+      {
+        folderId,
+        title,
+      },
+    );
+    return data.data.title;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const moveFolder = async (folderId: number, toId: number) => {
+  try {
+    const {data} = await axios.post<ApiResponse<null>>('folder/move', {
+      folderId: folderId,
+      toId: toId,
+    });
+    console.log(data);
   } catch (error) {
     throw error;
   }
