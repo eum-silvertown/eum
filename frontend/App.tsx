f2b97cb0c0a073d846966a0012b1a4a8163b085a
박효진 <jam_ty@naver.com>
2024-11-07 02:04:51 +0900

    chore : react-query 환경 구성

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SystemNavigationBar from 'react-native-system-navigation-bar';
-import {ScreenType} from '@store/useCurrentScreenStore';
+import { ScreenType } from '@store/useCurrentScreenStore';
 import LoginScreen from '@screens/LoginScreen';
 import FindIdScreen from '@screens/FindIdScreen';
 import FindPasswordScreen from '@screens/FindPasswordScreen';
@@ -11,13 +11,15 @@ import HomeScreen from '@screens/HomeScreen';
 import LessoningScreen from '@screens/LessoningScreen';
 import LectureListScreen from '@screens/LectureListScreen';
 import ClassListScreen from '@screens/ClassListScreen';
+import ClassDetailScreen from '@screens/ClassDetailScreen'
 import HomeworkScreen from '@screens/HomeworkScreen';
 import QuestionBoxScreen from '@screens/QuestionBoxScreen';
 import MyClassScreen from '@screens/MyClassScreen';
 import NotificationScreen from '@screens/NotificationScreen';
 import LessoningStudentListScreen from '@screens/LessoningStudentListScreen';
 import MainLayout from '@components/common/MainLayout';
-import {Platform, UIManager} from 'react-native';
+import { Platform, UIManager } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // 안드로이드 기본 Navbar 없애기
 SystemNavigationBar.stickyImmersive();
@@ -36,45 +38,50 @@ interface ScreenProps {
   component: () => React.JSX.Element;
 }
 
+const queryClient = new QueryClient();
+
 function App(): React.JSX.Element {
   const screens: ScreenProps[] = [
-    {name: 'LoginScreen', component: LoginScreen},
-    {name: 'FindIdScreen', component: FindIdScreen},
-    {name: 'FindPasswordScreen', component: FindPasswordScreen},
-    {name: 'SignUpSelectScreen', component: SignUpSelectScreen},
-    {name: 'SignUpScreen', component: SignUpScreen},
-    {name: 'HomeScreen', component: HomeScreen},
-    {name: 'ClassListScreen', component: ClassListScreen},
-    {name: 'HomeworkScreen', component: HomeworkScreen},
-    {name: 'QuestionBoxScreen', component: QuestionBoxScreen},
-    {name: 'MyClassScreen', component: MyClassScreen},
-    {name: 'NotificationScreen', component: NotificationScreen},
-    {name: 'LessoningScreen', component: LessoningScreen},
-    {name: 'LessoningStudentListScreen', component: LessoningStudentListScreen},
+    { name: 'LoginScreen', component: LoginScreen },
+    { name: 'FindIdScreen', component: FindIdScreen },
+    { name: 'FindPasswordScreen', component: FindPasswordScreen },
+    { name: 'SignUpSelectScreen', component: SignUpSelectScreen },
+    { name: 'SignUpScreen', component: SignUpScreen },
+    { name: 'HomeScreen', component: HomeScreen },
+    { name: 'ClassListScreen', component: LectureListScreen },
+    { name: 'ClassDetailScreen', component: ClassDetailScreen },
+    { name: 'HomeworkScreen', component: HomeworkScreen },
+    { name: 'QuestionBoxScreen', component: QuestionBoxScreen },
+    { name: 'MyClassScreen', component: MyClassScreen },
+    { name: 'NotificationScreen', component: NotificationScreen },
+    { name: 'LessoningScreen', component: LessoningScreen },
+    { name: 'LessoningStudentListScreen', component: LessoningStudentListScreen },
   ];
 
   return (
-    <NavigationContainer>
-      <MainLayout>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            animation: 'slide_from_right',
-            animationDuration: 200,
-            contentStyle: {
-              backgroundColor: 'transparent',
-            },
-          }}>
-          {screens.map((screen, index) => (
-            <Stack.Screen
-              key={index}
-              name={screen.name}
-              component={screen.component}
-            />
-          ))}
-        </Stack.Navigator>
-      </MainLayout>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <MainLayout>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+              animation: 'slide_from_right',
+              animationDuration: 200,
+              contentStyle: {
+                backgroundColor: 'transparent',
+              },
+            }}>
+            {screens.map((screen, index) => (
+              <Stack.Screen
+                key={index}
+                name={screen.name}
+                component={screen.component}
+              />
+            ))}
+          </Stack.Navigator>
+        </MainLayout>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
