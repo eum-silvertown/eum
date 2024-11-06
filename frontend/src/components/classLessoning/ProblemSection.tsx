9a842a38fbd6ff000be0f86dcde9ef1d2ff2c4f9
박효진 <jam_ty@naver.com>
2024-11-06 14:31:05 +0900

    feat : 문제 이미지 로딩 전까지 출력 안하는 로직 추가

--- frontend/src/components/classLessoning/ProblemSection.tsx
+++ frontend/src/components/classLessoning/ProblemSection.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MathJax from 'react-native-mathjax';
 import ZoomableView from './ZoomableView';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 
 type ProblemSectionProps = {
   problemText: string;
@@ -19,24 +19,32 @@ function ProblemSection({problemText}: ProblemSectionProps): React.JSX.Element {
   const removeImageMarkdown = (text: string): string => {
     return text.replace(/!\[.*?\]\(.*?\)/g, '');
   };
+
   const imageUrl = extractImageUrl(problemText);
   const textWithoutImage = removeImageMarkdown(problemText);
 
   const mathJaxContent = `
     <div style="padding: 24px;">
       <p>${textWithoutImage}</p>
-      ${
-        imageUrl
-          ? `<img src="${imageUrl}" style="width: 30%; margin-top: 24px; display: block; margin: 0 auto" />`
-          : ''
-      }
     </div>
   `;
+
+  // 이미지 로드 성공 여부를 상태로 관리
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
     <ZoomableView>
       <View style={styles.problemContainer}>
         <MathJax html={mathJaxContent} />
+        {imageUrl && (
+          <Image
+            source={{uri: imageUrl}}
+            style={[styles.image, imageLoaded ? null : styles.imageLoading]}
+            resizeMode="contain"
+            onLoad={() => setImageLoaded(true)}
+            onError={() => setImageLoaded(false)}
+          />
+        )}
       </View>
     </ZoomableView>
   );
@@ -48,5 +56,13 @@ const styles = StyleSheet.create({
   problemContainer: {
     width: '50%',
     alignSelf: 'center',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 3, // 이미지 비율
+    alignSelf: 'center',
+  },
+  imageLoading: {
+    display: 'none', // 이미지 로드 전까지 숨기기
   },
 });
