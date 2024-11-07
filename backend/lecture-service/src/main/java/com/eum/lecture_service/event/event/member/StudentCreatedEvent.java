acfacbadce538912cac1854cb239936eb87f2f48
이예찬 <dldpcks34@naver.com>
2024-11-07 12:45:06 +0900

    refactor(#S11P31D101-415): 카프카 회원 이벤트 발생 로직 추가

---
+++ backend/lecture-service/src/main/java/com/eum/lecture_service/event/event/member/StudentCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.eum.lecture_service.event.event.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentCreatedEvent {
+    private Long studentId;
+    private String name;
+    private String image;
+    private Long classId;
+    private Long grade;
+    private Long classNumber;
+}
