8a547b03b03b0417c4107e78a75be91710a1e199
김동준 <kdj4355@naver.com>
2024-11-07 09:04:04 +0900

    refactor: 로그인 인풋필드 수정 및 자동로그인 관련 기능 수정

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   TouchableOpacity,
   StyleSheet,
@@ -15,27 +15,62 @@ import serviceLogoImage from '@assets/images/serviceLogoImage.png';
 import {iconSize} from '@theme/iconSize';
 import {colors} from 'src/hooks/useColors';
 
-import {useAuthStore} from '@store/useAuthStore';
-
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {ScreenType, useCurrentScreenStore} from '@store/useCurrentScreenStore';
 
+import {logIn} from '@services/authService';
+import InputField from '@components/account/InputField';
+
+import {getToken, setAutoLogin, getAutoLoginStatus} from '@utils/secureStorage';
+import {useAuthStore} from '@store/useAuthStore';
 type NavigationProps = NativeStackNavigationProp<ScreenType>;
 
 function LoginScreen(): React.JSX.Element {
-  const [isChecked, setIsChecked] = useState(false); // 체크박스 상태 관리
-  const [passwordVisible, setPasswordVisible] = useState(false); // 비밀번호 표시 여부 상태
-  const login = useAuthStore(state => state.login); // login 메서드 가져오기
+  const [isChecked, setIsChecked] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const navigation = useNavigation<NavigationProps>();
   const {setCurrentScreen} = useCurrentScreenStore();
 
-  // 임시 로그인 함수
-  const handleLogin = () => {
-    // 임시 사용자 이름과 비밀번호를 사용해 로그인
-    login('tempUser', 'password123');
-    navigation.navigate('HomeScreen');
-    setCurrentScreen('HomeScreen');
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const setIsLoggedIn = useAuthStore((state) => state.setIsLoggedIn);
+
+  // 잠시 주석 처리
+
+  // useEffect(() => {
+  //   const initializeAuth = async () => {
+  //     // 자동 로그인 상태와 토큰 확인
+  //     const autoLoginEnabled = await getAutoLoginStatus();
+  //     const tokenData = await getToken();
+
+  //     console.log (autoLoginEnabled, tokenData)
+      
+  //     if (autoLoginEnabled && tokenData?.accessToken) {
+  //       // 토큰이 존재하고 자동 로그인이 활성화된 경우
+  //       setIsLoggedIn(true); // 스토어 상태를 로그인으로 설정
+  //       navigation.navigate('HomeScreen'); // 홈 화면으로 이동
+  //     } else {
+  //       setIsLoggedIn(false); // 스토어 상태를 로그아웃 상태로 설정
+  //       navigation.navigate('LoginScreen'); // 로그인 화면으로 이동
+  //     }
+  //   };
+
+  //   initializeAuth();
+  // }, []);
+
+  const handleLogin = async () => {
+    try {
+      const response = await logIn({id, password});
+
+      // 자동 로그인 설정 상태 저장
+      await setAutoLogin(isChecked);
+      console.log('auto', isChecked)
+      navigation.navigate('HomeScreen');
+      setCurrentScreen('HomeScreen');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const moveSignUp = () => {
@@ -46,12 +81,12 @@ function LoginScreen(): React.JSX.Element {
   const moveFindId = () => {
     navigation.navigate('FindIdScreen');
     setCurrentScreen('FindIdScreen');
-  }
+  };
 
   const moveFindPassword = () => {
     navigation.navigate('FindPasswordScreen');
     setCurrentScreen('FindPasswordScreen');
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -61,33 +96,31 @@ function LoginScreen(): React.JSX.Element {
       </View>
 
       <View style={styles.loginfield}>
-        <View style={styles.inputField}>
-          <Text weight="bold">아이디</Text>
-          <TextInput placeholder="아이디를 입력해주세요." />
-        </View>
-        <View style={styles.inputField}>
-          <Text weight="bold">비밀번호</Text>
-          <View style={styles.passwordContainer}>
-            <TextInput
-              placeholder="비밀번호를 입력해주세요."
-              secureTextEntry={!passwordVisible}
-              style={styles.passwordInput}
-            />
-            <TouchableOpacity
-              onPress={() => setPasswordVisible(!passwordVisible)} // 버튼을 눌렀을 때 상태 변경
-              >
-              {passwordVisible ? (
+        <View>
+          <InputField
+            label="아이디"
+            value={id}
+            onChangeText={setId}
+            placeholder="아이디를 입력해주세요."
+          />
+          <InputField
+            label="비밀번호"
+            value={password}
+            onChangeText={setPassword}
+            placeholder="비밀번호를 입력해주세요."
+            secureTextEntry={!passwordVisible}
+            iconComponent={
+              passwordVisible ? (
                 <PasswordVisibleIcon width={iconSize.md} height={iconSize.md} />
               ) : (
                 <PasswordVisibleOffIcon
                   width={iconSize.md}
                   height={iconSize.md}
                 />
-              )}
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.loginOptions}>
+              )
+            }
+            onIconPress={() => setPasswordVisible(!passwordVisible)}
+          />
           <View style={styles.checkboxContainer}>
             <TouchableOpacity
               onPress={() => setIsChecked(!isChecked)} // 클릭 시 체크박스 상태 변경
@@ -103,19 +136,25 @@ function LoginScreen(): React.JSX.Element {
         </View>
 
         <TouchableOpacity style={styles.submitButton} onPress={handleLogin}>
-          <Text style={styles.submitButtonText} color="white" weight='bold'>
+          <Text style={styles.submitButtonText} color="white" weight="bold">
             로그인
           </Text>
         </TouchableOpacity>
 
         <View style={styles.accountContainer}>
-          <TouchableOpacity style={styles.moveScreenButton} onPress={moveSignUp}>
+          <TouchableOpacity
+            style={styles.moveScreenButton}
+            onPress={moveSignUp}>
             <Text style={styles.submitButtonText}>회원가입</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.moveScreenButton} onPress={moveFindId}>
+          <TouchableOpacity
+            style={styles.moveScreenButton}
+            onPress={moveFindId}>
             <Text style={styles.submitButtonText}>아이디 찾기</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.moveScreenButton} onPress={moveFindPassword}>
+          <TouchableOpacity
+            style={styles.moveScreenButton}
+            onPress={moveFindPassword}>
             <Text style={styles.submitButtonText}>비밀번호 찾기</Text>
           </TouchableOpacity>
         </View>
@@ -129,6 +168,7 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
     paddingHorizontal: spacing.md,
     justifyContent: 'center',
@@ -147,7 +187,7 @@ const styles = StyleSheet.create({
   },
   passwordInput: {
     flex: 1,
-  },  
+  },
   loginOptions: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -175,7 +215,7 @@ const styles = StyleSheet.create({
   },
   accountContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',    
-    marginHorizontal: spacing.xl    
-  }
+    justifyContent: 'space-between',
+    marginHorizontal: spacing.xl,
+  },
 });
