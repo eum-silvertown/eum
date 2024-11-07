d5ed1718c21ab06728240c9b3d61b4be86543339
김동준 <kdj4355@naver.com>
2024-11-07 09:17:56 +0900

    feat: authStore 변경사항 추가

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,16 +1,23 @@
-import {create} from 'zustand';
-import * as Keychain from 'react-native-keychain';
+import { create } from 'zustand';
 
 interface AuthState {
   isLoggedIn: boolean;
   setIsLoggedIn: (status: boolean) => void;
   username: string | null;
-  role: string | null;  
+  setUsername: (username: string) => void;
+  role: string | null;
+  setRole: (role: string) => void;
+  userProfileImage: string | null;
+  setUserProfileImage: (image: string) => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn: false,
   setIsLoggedIn: (status) => set({ isLoggedIn: status }),
-  username: '',
-  role: '',
+  username: null,
+  setUsername: (username) => set({ username }),
+  role: null,
+  setRole: (role) => set({ role }),
+  userProfileImage: null,
+  setUserProfileImage: (image) => set({ userProfileImage: image }),
 }));
