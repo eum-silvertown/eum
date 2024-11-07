f468e42f29eaae438e5fd6fdd269b710f301243f
김동준 <kdj4355@naver.com>
2024-11-07 22:49:23 +0900

    feat: authStore 유저 정보 저장 기능 추가

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,23 +1,56 @@
-import { create } from 'zustand';
+import {create} from 'zustand';
+
+interface ClassInfo {
+  school: string;
+  grade: number;
+  classNumber: number;
+}
+
+interface UserInfo {
+  id: number;
+  name: string;
+  classInfo: ClassInfo;
+  birth: string;
+  image: string | null;
+}
 
 interface AuthState {
   isLoggedIn: boolean;
   setIsLoggedIn: (status: boolean) => void;
+  
   username: string | null;
   setUsername: (username: string) => void;
   role: string | null;
   setRole: (role: string) => void;
   userProfileImage: string | null;
   setUserProfileImage: (image: string) => void;
+
+  userInfo: UserInfo; 
+  setUserInfo: (info: UserInfo) => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
+// 초기값으로 사용할 빈 객체
+const initialUserInfo: UserInfo = {
+  id: 0,
+  name: '',
+  classInfo: {
+    school: '',
+    grade: 0,
+    classNumber: 0,
+  },
+  birth: '',
+  image: null,
+};
+
+export const useAuthStore = create<AuthState>(set => ({
   isLoggedIn: false,
-  setIsLoggedIn: (status) => set({ isLoggedIn: status }),
+  setIsLoggedIn: status => set({isLoggedIn: status}),
   username: null,
-  setUsername: (username) => set({ username }),
+  setUsername: username => set({username}),
   role: null,
-  setRole: (role) => set({ role }),
+  setRole: role => set({role}),
   userProfileImage: null,
-  setUserProfileImage: (image) => set({ userProfileImage: image }),
+  setUserProfileImage: image => set({userProfileImage: image}),
+  userInfo: initialUserInfo,
+  setUserInfo: info => set({userInfo: info}),
 }));
