6f18dda75ea31da749b4f0eef54dbde1e2feefed
김동준 <kdj4355@naver.com>
2024-11-08 22:23:46 +0900

    fix: 커스텀 드랍다운 피커 스타일 추가

--- frontend/src/components/common/CustomDropdownPicker.tsx
+++ frontend/src/components/common/CustomDropdownPicker.tsx
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { StyleSheet, View } from 'react-native';
-import { spacing } from '@theme/spacing';
-import { colors } from 'src/hooks/useColors';
-import { borderWidth } from '@theme/borderWidth';
-import { getResponsiveSize } from '@utils/responsive';
+import {StyleSheet, View, ViewStyle} from 'react-native';
+import {spacing} from '@theme/spacing';
+import {colors} from 'src/hooks/useColors';
+import {borderWidth} from '@theme/borderWidth';
+import {getResponsiveSize} from '@utils/responsive';
+import {Text} from '@components/common/Text';
 
 interface CustomDropdownPickerProps {
-  items: { label: string; value: string }[]; // 드롭다운 항목
-  placeholder?: string;                      // 기본 표시 텍스트
-  onSelectItem: (value: string) => void;     // 선택 항목 변경 시 콜백
-  defaultValue?: string;                     // 초기 선택 값
+  label?: string;
+  items: {label: string; value: string}[]; // 드롭다운 항목
+  placeholder?: string; // 기본 표시 텍스트
+  onSelectItem: (value: string) => void; // 선택 항목 변경 시 콜백
+  defaultValue?: string; // 초기 선택 값
+  containerStyle?: ViewStyle; // 컨테이너 스타일
+  dropdownStyle?: ViewStyle; // 드롭다운 스타일  
 }
 
 const CustomDropdownPicker = ({
+  label,
   items,
   placeholder = '선택하세요', // 기본 플레이스홀더
   onSelectItem,
   defaultValue,
-}: CustomDropdownPickerProps) => {
+  containerStyle,
+  dropdownStyle,
+}: CustomDropdownPickerProps) : React.JSX.Element => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(defaultValue || null);
 
@@ -28,15 +35,16 @@ const CustomDropdownPicker = ({
   };
 
   return (
-    <View style={styles.container}>
-      <DropDownPicker
+    <View style={[styles.container, containerStyle]}>
+      {label && <Text variant='subtitle' weight='bold' style={styles.label}>{label}</Text>}
+      <DropDownPicker      
         open={open}
         value={value}
         items={items}
         setOpen={setOpen}
         setValue={handleSelectItem}
         placeholder={placeholder}
-        style={styles.dropdownStyle}
+        style={[styles.dropdownStyle, dropdownStyle]}
         dropDownContainerStyle={styles.dropDownContainerStyle}
         textStyle={styles.textStyle}
         placeholderStyle={styles.placeholderStyle}
@@ -54,7 +62,7 @@ const styles = StyleSheet.create({
   dropdownStyle: {
     backgroundColor: 'white',
     borderColor: colors.light.background.main,
-    borderWidth: borderWidth.xs,    
+    borderWidth: borderWidth.xs,
   },
   dropDownContainerStyle: {
     backgroundColor: 'white',
@@ -72,8 +80,11 @@ const styles = StyleSheet.create({
     color: colors.light.background.main,
     fontSize: getResponsiveSize(8),
   },
-  selectedItemLabelStyle: {    
+  selectedItemLabelStyle: {
     color: '#007AFF',
+  },
+  label: {
+    marginBottom: spacing.sm,
   },
 });
 
