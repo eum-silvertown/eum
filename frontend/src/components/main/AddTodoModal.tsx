690bf928adce94ed94001d5cffdc9b15e6d09f64
김동준 <kdj4355@naver.com>
2024-11-10 20:53:17 +0900

    feat: 투두생성, 수정 모달 제작

--- frontend/src/components/main/AddTodoModal.tsx
+++ frontend/src/components/main/AddTodoModal.tsx
@@ -1,26 +1,105 @@
-// AddTodoModal.tsx
-import React from 'react';
-import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
 import {Text} from '@components/common/Text';
 import InputField from '@components/account/InputField';
 import {spacing} from '@theme/spacing';
 import {colors} from 'src/hooks/useColors';
 import {useModalContext} from 'src/contexts/useModalContext';
+import {createTodo, updateTodo} from '@services/todoService';
 
-const AddTodoModal = (): React.JSX.Element => {
-  const importanceLevels = ['일반', '평범', '중요', '매우 중요'];
+interface AddTodoModalProps {
+  onTodoListUpdate?: () => void;
+  isEditMode?: boolean;
+  todo?: {
+    id: number;
+    title: string;
+    content: string;
+    prioirty: number;
+  };
+}
+
+const importanceLevels = ['일반', '평범', '중요', '매우 중요'];
+
+// 중요도에 따른 색상 반환 함수
+const getCheckBoxColor = (importance: number, isSelected: boolean) => {
+  const color = isSelected
+    ? {1: '#4CAF50', 2: '#FF9800', 3: '#F44336'}[importance] || '#2E2559'
+    : {1: '#A5D6A7', 2: '#FFCC80', 3: '#EF9A9A'}[importance] || '#D1C4E9';
+  return color;
+};
+
+const AddTodoModal = ({
+  onTodoListUpdate,
+
+  isEditMode = false,
+  todo,
+}: AddTodoModalProps): React.JSX.Element => {
+  const [title, setTitle] = useState(todo?.title || '');
+  const [content, setContent] = useState(todo?.content || '');
+  const [selectedImportance, setSelectedImportance] = useState(
+    todo?.prioirty || 0,
+  );
   const {close} = useModalContext();
-  const createTodo = () => {
-    // TODO: 할 일 생성 로직
+
+  const [titleStatusText, setTitleStatusText] = useState('');
+  const [contentStatusText, setContentStatusText] = useState('');
+
+  const handleSaveTodo = async () => {
+    let hasError = false;
+
+    if (!title.trim()) {
+      setTitleStatusText('제목을 입력해주세요.');
+      hasError = true;
+    } else {
+      setTitleStatusText('');
+    }
+
+    if (!content.trim()) {
+      setContentStatusText('내용을 입력해주세요.');
+      hasError = true;
+    } else {
+      setContentStatusText('');
+    }
+
+    if (hasError) return;
+
+    try {
+      const response = isEditMode
+        ? await updateTodo(todo?.id!, {
+            title,
+            content,
+            prioirty: selectedImportance,
+          })
+        : await createTodo({title, content, prioirty: selectedImportance});
+
+      console.log(isEditMode ? '투두 수정 성공' : '투두 생성 성공', response);
+
+      if (onTodoListUpdate) {
+        onTodoListUpdate(); // 정의된 경우에만 호출
+      }
+
+      close(); // 모달 닫기
+    } catch (error) {
+      Alert.alert(
+        isEditMode
+          ? '할 일 수정에 실패하였습니다.'
+          : '할 일 생성을 실패하였습니다.',
+      );
+    }
   };
 
   return (
     <View>
-      <View style={[styles.contentContainer]}>
-        <Text variant="subtitle" weight="bold">
-          제목
-        </Text>
-        <InputField placeholder="제목을 입력해주세요." />
+      <View style={styles.contentContainer}>
+        <InputField
+          label="제목"
+          placeholder="제목을 입력해주세요."
+          value={title}
+          onChangeText={setTitle}
+          maxLength={20}
+          status="error"
+          statusText={titleStatusText}
+        />
       </View>
 
       <View style={styles.contentContainer}>
@@ -29,7 +108,18 @@ const AddTodoModal = (): React.JSX.Element => {
         </Text>
         <View style={styles.importanceLevelContainer}>
           {importanceLevels.map((item, index) => (
-            <TouchableOpacity key={index} style={styles.importanceButton}>
+            <TouchableOpacity
+              key={index}
+              onPress={() => setSelectedImportance(index)}
+              style={[
+                styles.importanceButton,
+                {
+                  backgroundColor: getCheckBoxColor(
+                    index,
+                    selectedImportance === index,
+                  ),
+                },
+              ]}>
               <Text color="white" weight="bold" align="center">
                 {item}
               </Text>
@@ -38,16 +128,21 @@ const AddTodoModal = (): React.JSX.Element => {
         </View>
       </View>
 
-      <View style={[styles.contentContainer]}>
-        <Text variant="subtitle" weight="bold">
-          내용
-        </Text>
-        <InputField placeholder="내용을 입력해주세요." />
+      <View style={styles.contentContainer}>
+        <InputField
+          label="내용"
+          placeholder="내용을 입력해주세요."
+          value={content}
+          onChangeText={setContent}
+          maxLength={50}
+          status="error"
+          statusText={contentStatusText}
+        />
       </View>
 
-      <TouchableOpacity onPress={createTodo} style={styles.submitButton}>
+      <TouchableOpacity onPress={handleSaveTodo} style={styles.submitButton}>
         <Text color="white" weight="bold" align="center">
-          생성
+          {isEditMode ? '수정' : '생성'}
         </Text>
       </TouchableOpacity>
     </View>
@@ -65,7 +160,6 @@ const styles = StyleSheet.create({
   },
   importanceButton: {
     flex: 1,
-    backgroundColor: colors.light.background.main,
     padding: spacing.md,
     borderRadius: 5,
     alignItems: 'center',
