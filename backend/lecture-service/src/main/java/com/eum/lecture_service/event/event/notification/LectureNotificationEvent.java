f1eee6ee790b15a8fc8d1c45deafc8f978305cb6
이예찬 <dldpcks34@naver.com>
2024-11-11 16:30:10 +0900

    fix(#S11P31D101-415): 역직열화 문제 해결

--- backend/lecture-service/src/main/java/com/eum/lecture_service/event/event/notification/LectureNotificationEvent.java
+++ backend/lecture-service/src/main/java/com/eum/lecture_service/event/event/notification/LectureNotificationEvent.java
@@ -3,11 +3,15 @@ package com.eum.lecture_service.event.event.notification;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LectureNotificationEvent implements Serializable {
     private Long lectureId;
     private String lectureTitle;
