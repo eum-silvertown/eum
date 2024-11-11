0ef72140ab4791705cc942cb2efc58dc1b5299b9
이지언 <dlwldjs7544@naver.com>
2024-11-11 16:18:25 +0900

    fix(#S11P31D101-472) : 문제 저장 시, 제목 중복에 관한 버그 수정

--- backend/folder-service/src/main/java/com/eum/folderservice/domain/savedFile/api/SavedFileController.java
+++ backend/folder-service/src/main/java/com/eum/folderservice/domain/savedFile/api/SavedFileController.java
@@ -5,6 +5,7 @@ import com.eum.folderservice.domain.savedFile.dto.request.SavedFileModifyRequest
 import com.eum.folderservice.domain.savedFile.dto.request.SavedFileMoveRequestDTO;
 import com.eum.folderservice.global.util.CommonResponse;
 import com.eum.folderservice.domain.savedFile.service.SavedFileService;
+import io.micrometer.core.annotation.Timed;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class SavedFileController {
     private final SavedFileService savedFileService;
 
     @PostMapping
+    @Timed(value = "files.create", longTask = true)
     public CommonResponse<?> createFile(@RequestBody SavedFileCreateRequestDTO requestDTO, HttpServletRequest request) {
         Long memberId = Long.parseLong(request.getHeader("X-MEMBER-ID"));
         requestDTO.setMemberId(memberId);
@@ -25,22 +27,26 @@ public class SavedFileController {
     }
 
     @GetMapping("/{fileId}")
+    @Timed(value = "files.get", longTask = true)
     public CommonResponse<?> getFileDetails(@PathVariable Long fileId) {
         return CommonResponse.success(savedFileService.getSavedFileDetail(fileId), "문제 상세 조회 성공");
     }
 
-    @PutMapping()
+    @PutMapping
+    @Timed(value = "files.modify", longTask = true)
     public CommonResponse<?> modifyFile(@RequestBody SavedFileModifyRequestDTO requestDTO) {
         return CommonResponse.success(savedFileService.modifySavedFile(requestDTO), "문제 수정 성공");
     }
 
     @DeleteMapping("/{fileId}")
+    @Timed(value = "files.delete", longTask = true)
     public CommonResponse<?> deleteFile(@PathVariable Long fileId) {
         savedFileService.deleteSavedFile(fileId);
         return CommonResponse.success(null, "문제 삭제 성공");
     }
 
     @PostMapping("/move")
+    @Timed(value = "files.move", longTask = true)
     public CommonResponse<?> moveFolder(@RequestBody SavedFileMoveRequestDTO requestDTO) {
         savedFileService.moveFolder(requestDTO);
         return CommonResponse.success(null, "문제 이동 성공");
