645170fa5a312956b10ac153047972892360c0d4
손민락 <dungguk@naver.com>
2024-11-14 14:35:42 +0900

    feat: firebase 알림 메세지 받는 기능 추가

--- frontend/index.js
+++ frontend/index.js
@@ -7,10 +7,15 @@ import App from './App';
 import {name as appName} from './app.json';
 import axios from 'axios';
 import Config from 'react-native-config';
+import messaging from '@react-native-firebase/messaging';
 
 // baseURL 설정
 axios.defaults.baseURL = Config.BACKEND_API_URL;
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 5000;
+
+messaging().setBackgroundMessageHandler(async msg => {
+  console.log(msg)  
+});
 
 AppRegistry.registerComponent(appName, () => App);
