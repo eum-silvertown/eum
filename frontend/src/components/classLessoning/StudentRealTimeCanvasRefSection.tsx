a2614e331f9d1af5b31fb73368b7dcc94b49896a
박효진 <jam_ty@naver.com>
2024-11-14 14:04:46 +0900

    test : 좌표 보정 테스트

--- frontend/src/components/classLessoning/StudentRealTimeCanvasRefSection.tsx
+++ frontend/src/components/classLessoning/StudentRealTimeCanvasRefSection.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Canvas, Path, Skia, useCanvasRef } from '@shopify/react-native-skia';
+import {useEffect, useState} from 'react';
+import {Canvas, Path, Skia, useCanvasRef} from '@shopify/react-native-skia';
 import pako from 'pako';
-import { StyleSheet, View } from 'react-native';
+import {Dimensions, StyleSheet, View, Text} from 'react-native';
 import base64 from 'react-native-base64';
 
 interface StudentCanvasSectionProps {
@@ -21,28 +21,51 @@ function StudentRealTimeCanvasRefSection({
 }: StudentCanvasSectionProps): React.JSX.Element {
   const canvasRef = useCanvasRef();
   const [paths, setPaths] = useState<PathData[]>([]);
+  const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
+
+  // 기기 비율 계산을 위한 상태 변수
+  const [widthRatio, setWidthRatio] = useState(1);
+  const [heightRatio, setHeightRatio] = useState(1);
 
   useEffect(() => {
     if (receivedMessage) {
       const messageObject = JSON.parse(receivedMessage);
-      if (messageObject.drawingData) {
+      if (
+        messageObject.drawingData &&
+        messageObject.width &&
+        messageObject.height
+      ) {
+        const newWidthRatio = deviceWidth / messageObject.width;
+        const newHeightRatio = deviceHeight / messageObject.height;
+
+        setWidthRatio(newWidthRatio);
+        setHeightRatio(newHeightRatio);
+
+        console.log('Calculated widthRatio:', newWidthRatio);
+        console.log('Calculated heightRatio:', newHeightRatio);
+
         handleSync(messageObject.drawingData);
         handleSyncMove(messageObject.drawingData);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [receivedMessage]);
+  }, [receivedMessage, deviceWidth, deviceHeight]);
 
   const handleSync = (base64EncodedData: string) => {
     try {
       const binaryString = base64.decode(base64EncodedData);
-      const compressedData = Uint8Array.from(binaryString.split('').map(char => char.charCodeAt(0)));
-      const decompressedData = JSON.parse(pako.inflate(compressedData, { to: 'string' }));
+      const compressedData = Uint8Array.from(
+        binaryString.split('').map(char => char.charCodeAt(0)),
+      );
+      const decompressedData = JSON.parse(
+        pako.inflate(compressedData, {to: 'string'}),
+      );
 
       const parsedPaths = decompressedData
         .map((pathData: any) => {
-          const path = Skia.Path.MakeFromSVGString(pathData.path);
-          return path ? { ...pathData, path } : null;
+          const pathString = pathData.path;
+          const path = Skia.Path.MakeFromSVGString(pathString);
+          return path ? {...pathData, path} : null;
         })
         .filter(Boolean);
 
@@ -55,16 +78,48 @@ function StudentRealTimeCanvasRefSection({
   const handleSyncMove = (base64EncodedData: string) => {
     try {
       const binaryString = base64.decode(base64EncodedData);
-      const compressedData = Uint8Array.from(binaryString.split('').map(char => char.charCodeAt(0)));
-      const newPathData = JSON.parse(pako.inflate(compressedData, { to: 'string' }));
-      const newPath = Skia.Path.MakeFromSVGString(newPathData.path);
+      const compressedData = Uint8Array.from(
+        binaryString.split('').map(char => char.charCodeAt(0)),
+      );
+      const newPathData = JSON.parse(
+        pako.inflate(compressedData, {to: 'string'}),
+      );
+
+      const adjustedPathString = adjustPathToCurrentResolution(
+        newPathData.path,
+        widthRatio,
+        heightRatio,
+      );
+      const newPath = Skia.Path.MakeFromSVGString(adjustedPathString);
 
       if (newPath) {
-        setPaths(prevPaths => mergeSimilarPaths([...prevPaths, { ...newPathData, path: newPath }]));
+        setPaths(prevPaths =>
+          mergeSimilarPaths([...prevPaths, {...newPathData, path: newPath}]),
+        );
       }
     } catch (error) {
       console.error('Failed to decompress or parse data:', error);
     }
+  };
+
+  const adjustPathToCurrentResolution = (
+    pathString: string,
+    reWidthRatio: number,
+    reHeightRatio: number,
+  ): string => {
+    return pathString
+      .split(' ')
+      .map(segment => {
+        const isCoordinate = !isNaN(parseFloat(segment));
+        if (isCoordinate) {
+          const number = parseFloat(segment);
+          return (
+            number * (segment.includes('y') ? reHeightRatio : reWidthRatio)
+          ).toFixed(4);
+        }
+        return segment;
+      })
+      .join(' ');
   };
 
   const mergeSimilarPaths = (newPaths: PathData[]): PathData[] => {
@@ -79,11 +134,14 @@ function StudentRealTimeCanvasRefSection({
         lastMergedPath.strokeWidth === currentPath.strokeWidth &&
         lastMergedPath.opacity === currentPath.opacity
       ) {
-        const mergedPathString = lastMergedPath.path.toSVGString() + ' ' + currentPath.path.toSVGString();
+        const mergedPathString =
+          lastMergedPath.path.toSVGString() +
+          ' ' +
+          currentPath.path.toSVGString();
         const mergedPath = Skia.Path.MakeFromSVGString(mergedPathString);
 
         if (mergedPath) {
-          lastMergedPath.path = mergedPath; // 병합된 경로 업데이트
+          lastMergedPath.path = mergedPath;
         }
       } else {
         mergedPaths.push(currentPath);
@@ -95,8 +153,20 @@ function StudentRealTimeCanvasRefSection({
 
   return (
     <View style={styles.canvasLayout}>
-      <Canvas style={styles.canvas} ref={canvasRef}>
-        {paths.map(({ path, color, strokeWidth, opacity }, index) => (
+      <Canvas
+        style={[
+          styles.canvas,
+          {
+            transform: [
+              {translateX: (deviceWidth * (1 - 1 / widthRatio)) / 2},
+              {translateY: (deviceHeight * (1 - 1 / heightRatio)) / 2},
+              {scaleX: 1 / widthRatio},
+              {scaleY: 1 / heightRatio},
+            ],
+          },
+        ]}
+        ref={canvasRef}>
+        {paths.map(({path, color, strokeWidth, opacity}, index) => (
           <Path
             key={index}
             path={path}
@@ -109,6 +179,14 @@ function StudentRealTimeCanvasRefSection({
           />
         ))}
       </Canvas>
+      <View style={styles.debugContainer}>
+        <Text style={styles.debugText}>
+          Width Ratio: {widthRatio.toFixed(4)}
+        </Text>
+        <Text style={styles.debugText}>
+          Height Ratio: {heightRatio.toFixed(4)}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -124,5 +202,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'transparent',
   },
-  canvas: { flex: 1 },
+  canvas: {flex: 1},
+  debugContainer: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 10,
+    borderRadius: 5,
+  },
+  debugText: {
+    color: 'white',
+    fontSize: 12,
+  },
 });
--- frontend/src/components/classLessoning/TeacherRealTimeCanvasRefSection.tsx
+++ frontend/src/components/classLessoning/TeacherRealTimeCanvasRefSection.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Canvas, Path, Skia, useCanvasRef} from '@shopify/react-native-skia';
 import pako from 'pako';
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import base64 from 'react-native-base64';
 
 interface StudentCanvasSectionProps {
@@ -15,26 +15,40 @@ type PathData = {
   opacity: number;
 };
 
-// 학생 캔버스 컴포넌트
 function TeacherRealTimeCanvasRefSection({
   receivedMessage,
 }: StudentCanvasSectionProps): React.JSX.Element {
   const canvasRef = useCanvasRef();
   const [paths, setPaths] = useState<PathData[]>([]);
-  console.log('학생 캔버스 받을 준비 완료');
+  const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
+
+  // 보정치를 위한 상태 변수
+  const [widthRatio, setWidthRatio] = useState(1);
+  const [heightRatio, setHeightRatio] = useState(1);
 
   useEffect(() => {
     if (receivedMessage) {
-      console.log('receivedMessage', receivedMessage);
-
       const messageObject = JSON.parse(receivedMessage);
-      if (messageObject.drawingData) {
+      if (
+        messageObject.drawingData &&
+        messageObject.width &&
+        messageObject.height
+      ) {
+        const newWidthRatio = deviceWidth / messageObject.width;
+        const newHeightRatio = deviceHeight / messageObject.height;
+
+        setWidthRatio(newWidthRatio);
+        setHeightRatio(newHeightRatio);
+
+        console.log('Calculated widthRatio:', newWidthRatio);
+        console.log('Calculated heightRatio:', newHeightRatio);
+
         handleSync(messageObject.drawingData);
         handleSyncMove(messageObject.drawingData);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [receivedMessage]);
+  }, [receivedMessage, deviceWidth, deviceHeight]);
 
   const handleSync = (base64EncodedData: string) => {
     try {
@@ -48,7 +62,8 @@ function TeacherRealTimeCanvasRefSection({
 
       const parsedPaths = decompressedData
         .map((pathData: any) => {
-          const path = Skia.Path.MakeFromSVGString(pathData.path);
+          const pathString = pathData.path;
+          const path = Skia.Path.MakeFromSVGString(pathString);
           return path ? {...pathData, path} : null;
         })
         .filter(Boolean);
@@ -68,7 +83,13 @@ function TeacherRealTimeCanvasRefSection({
       const newPathData = JSON.parse(
         pako.inflate(compressedData, {to: 'string'}),
       );
-      const newPath = Skia.Path.MakeFromSVGString(newPathData.path);
+
+      const adjustedPathString = adjustPathToCurrentResolution(
+        newPathData.path,
+        widthRatio,
+        heightRatio,
+      );
+      const newPath = Skia.Path.MakeFromSVGString(adjustedPathString);
 
       if (newPath) {
         setPaths(prevPaths =>
@@ -78,6 +99,26 @@ function TeacherRealTimeCanvasRefSection({
     } catch (error) {
       console.error('Failed to decompress or parse data:', error);
     }
+  };
+
+  const adjustPathToCurrentResolution = (
+    pathString: string,
+    reWidthRatio: number,
+    reHeightRatio: number,
+  ): string => {
+    return pathString
+      .split(' ')
+      .map(segment => {
+        const isCoordinate = !isNaN(parseFloat(segment));
+        if (isCoordinate) {
+          const number = parseFloat(segment);
+          return (
+            number * (segment.includes('y') ? reHeightRatio : reWidthRatio)
+          ).toFixed(4);
+        }
+        return segment;
+      })
+      .join(' ');
   };
 
   const mergeSimilarPaths = (newPaths: PathData[]): PathData[] => {
@@ -99,7 +140,7 @@ function TeacherRealTimeCanvasRefSection({
         const mergedPath = Skia.Path.MakeFromSVGString(mergedPathString);
 
         if (mergedPath) {
-          lastMergedPath.path = mergedPath; // 병합된 경로 업데이트
+          lastMergedPath.path = mergedPath;
         }
       } else {
         mergedPaths.push(currentPath);
@@ -111,20 +152,40 @@ function TeacherRealTimeCanvasRefSection({
 
   return (
     <View style={styles.canvasLayout}>
-      <Canvas style={styles.canvas} ref={canvasRef}>
+      <Canvas
+        style={[
+          styles.canvas,
+          {
+            transform: [
+              {translateX: (deviceWidth * (1 - 1 / widthRatio)) / 2},
+              {translateY: (deviceHeight * (1 - 1 / heightRatio)) / 2},
+              {scaleX: 1 / widthRatio},
+              {scaleY: 1 / heightRatio},
+            ],
+          },
+        ]}
+        ref={canvasRef}>
         {paths.map(({path, color, strokeWidth, opacity}, index) => (
           <Path
             key={index}
             path={path}
             color={Skia.Color(color)}
             style="stroke"
-            strokeWidth={strokeWidth}
+            strokeWidth={strokeWidth * widthRatio}
             strokeCap="round"
             strokeJoin="round"
             opacity={opacity}
           />
         ))}
       </Canvas>
+      <View style={styles.debugContainer}>
+        <Text style={styles.debugText}>
+          Width Ratio: {widthRatio.toFixed(4)}
+        </Text>
+        <Text style={styles.debugText}>
+          Height Ratio: {heightRatio.toFixed(4)}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -141,4 +202,16 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   canvas: {flex: 1},
+  debugContainer: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 10,
+    borderRadius: 5,
+  },
+  debugText: {
+    color: 'white',
+    fontSize: 12,
+  },
 });
