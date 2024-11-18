5265759d3c5c796f09ecef75eac0bcedf15ed602
정진규 <wlsrb6905@naver.com>
2024-11-18 11:01:22 +0900

    refactor(#S11P31D101-540) : 유저 이미지 변경 이벤트 발송 dto & 로직 수정

--- backend/user-service/src/main/java/com/eum/user_service/domain/event/service/EventProducer.java
+++ backend/user-service/src/main/java/com/eum/user_service/domain/event/service/EventProducer.java
@@ -1,16 +1,14 @@
 package com.eum.user_service.domain.event.service;
 
-import com.eum.user_service.domain.event.dto.ClassEvent;
-import com.eum.user_service.domain.event.dto.StudentInfoEvent;
-import com.eum.user_service.domain.event.dto.TeacherInfoEvent;
+import com.eum.user_service.domain.event.dto.*;
 import com.eum.user_service.domain.user.entity.Member;
 
 public interface EventProducer {
 
     void sendMemberCreatedEvent(Member member);
     void sendTeacherCreatedEvent(TeacherInfoEvent event);
-    void sendTeacherUpdatedEvent(TeacherInfoEvent event);
+    void sendTeacherUpdatedEvent(TeacherImageEvent event);
     void sendClassCreatedEvent(ClassEvent event);
     void sendStudentCreatedEvent(StudentInfoEvent event);
-    void sendStudentUpdatedEvent(StudentInfoEvent event);
+    void sendStudentUpdatedEvent(StudentImageEvent event);
 }
