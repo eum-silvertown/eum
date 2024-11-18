9e2c6f710fd50de26c9780cbdb70a21ac8914838
박효진 <jam_ty@naver.com>
2024-11-18 12:27:15 +0900

    feat : 학생 점수 감소 로직 추가

--- frontend/src/screens/LessoningStudentListScreen.tsx
+++ frontend/src/screens/LessoningStudentListScreen.tsx
@@ -1,48 +1,51 @@
 import ParticipantCard from '@components/classLessoning/ParticipantCard';
-import { useEffect, useRef, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, Alert } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
-import { useCurrentScreenStore } from '@store/useCurrentScreenStore';
+import {useEffect, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
+import {useCurrentScreenStore} from '@store/useCurrentScreenStore';
 import TeacherLessoningGridInteractionTool from '@components/classLessoning/TeacherLessoningGridInteractionTool';
-import { getResponsiveSize } from '@utils/responsive';
+import {getResponsiveSize} from '@utils/responsive';
 import LeftIcon from '@assets/icons/leftIcon.svg';
 import RightIcon from '@assets/icons/rightIcon.svg';
 import LeftOffIcon from '@assets/icons/leftOffIcon.svg';
 import RightOffIcon from '@assets/icons/rightOffIcon.svg';
-import { iconSize } from '@theme/iconSize';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { ScreenType } from '@store/useCurrentScreenStore';
-import { useLectureStore, useLessonStore } from '@store/useLessonStore';
-import { useStudentListStore } from '@store/useStudentListStore';
+import {iconSize} from '@theme/iconSize';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {ScreenType} from '@store/useCurrentScreenStore';
+import {useLectureStore, useLessonStore} from '@store/useLessonStore';
+import {useStudentListStore} from '@store/useStudentListStore';
 import SockJS from 'sockjs-client';
 import * as StompJs from '@stomp/stompjs';
 import EmptyData from '@components/common/EmptyData';
-
+import {useMutation} from '@tanstack/react-query';
+import {adjustStudentAttitudeScore} from '@services/lectureInformation';
 
 type NavigationProps = NativeStackNavigationProp<ScreenType>;
 
 function LessoningStudentListScreen(): React.JSX.Element {
   const setCurrentScreen = useCurrentScreenStore(
-    (state) => state.setCurrentScreen
+    state => state.setCurrentScreen,
   );
   const navigation = useNavigation<NavigationProps>();
   useFocusEffect(() => {
     setCurrentScreen('LessoningStudentListScreen');
   });
+  const {participants, addParticipant, updateParticipant, removeParticipant} =
+    useStudentListStore();
 
-  const {
-    participants,
-    addParticipant,
-    updateParticipant,
-    removeParticipant,
-  } = useStudentListStore();
-
-  const lectureId = useLessonStore((state) => state.lectureId);
-  const lessonId = useLessonStore((state) => state.lessonId);
-  const teacherId = useLectureStore((state) => state.teacherId);
-  const setLectureInfo = useLectureStore((state) => state.setLectureInfo);
-  const setIsTeaching = useLessonStore((state) => state.setIsTeaching);
+  const lectureId = useLessonStore(state => state.lectureId);
+  const lessonId = useLessonStore(state => state.lessonId);
+  const teacherId = useLectureStore(state => state.teacherId);
+  const setLectureInfo = useLectureStore(state => state.setLectureInfo);
+  const setIsTeaching = useLessonStore(state => state.setIsTeaching);
 
   const clientRef = useRef<StompJs.Client | null>(null);
 
@@ -56,7 +59,7 @@ function LessoningStudentListScreen(): React.JSX.Element {
 
   const currentParticipants = participants.slice(
     teacherCurrentPage * PARTICIPANTS_PER_PAGE,
-    (teacherCurrentPage + 1) * PARTICIPANTS_PER_PAGE
+    (teacherCurrentPage + 1) * PARTICIPANTS_PER_PAGE,
   );
 
   const goToPreviousPage = () => {
@@ -77,25 +80,58 @@ function LessoningStudentListScreen(): React.JSX.Element {
     navigation.navigate('LessoningScreen');
   };
 
+  const mutation = useMutation({
+    mutationKey: ['adjustStudentAttitudeScore'], // 명시적으로 키 설정
+    mutationFn: async (studentId: number) => {
+      return adjustStudentAttitudeScore(lectureId!, studentId);
+    },
+    onSuccess: data => {
+      console.log('점수 감소 성공:', data);
+      Alert.alert('점수 감소 완료');
+    },
+    onError: error => {
+      console.error('점수 감소 실패:', error);
+      Alert.alert('점수 감소 실패', '다시 시도해주세요.');
+    },
+  });
+
   const handleLongPress = (studentId: number) => {
-    Alert.alert('학생 선택', `선택된 학생 ID: ${studentId}`);
+    Alert.alert(
+      '점수 감소',
+      `학생 ID ${studentId}의 점수를 감소시키겠습니까?`,
+      [
+        {
+          text: '취소',
+          style: 'cancel',
+        },
+        {
+          text: '확인',
+          onPress: () => {
+            // Mutation 실행
+            mutation.mutate(studentId);
+          },
+        },
+      ],
+    );
   };
 
   // STOMP 클라이언트 초기화 및 설정
   useEffect(() => {
     const client = new StompJs.Client({
-      webSocketFactory: () => new SockJS('http://k11d101.p.ssafy.io/ws-gateway/drawing'),
-      debug: (str) => console.log('STOMP Debug:', str),
+      webSocketFactory: () =>
+        new SockJS('http://k11d101.p.ssafy.io/ws-gateway/drawing'),
+      debug: str => console.log('STOMP Debug:', str),
       reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
+      heartbeatIncoming: 1000000,
+      heartbeatOutgoing: 1000000,
       onConnect: () => {
         console.log('STOMP client successfully connected');
 
         const teacherTopic = `/topic/lesson/${lessonId}`;
-        client.subscribe(teacherTopic, (message) => {
+        client.subscribe(teacherTopic, message => {
           const data = JSON.parse(message.body);
-          const { type, studentId, studentName, studentImage, currentPage } = data;
+          const {type, studentId, studentName, studentImage, currentPage} =
+            data;
 
           if (type === 'in') {
             addParticipant({
@@ -106,14 +142,14 @@ function LessoningStudentListScreen(): React.JSX.Element {
               status: 'in',
             });
           } else if (type === 'now') {
-            updateParticipant(studentId, { status: 'now', currentPage });
+            updateParticipant(studentId, {status: 'now', currentPage});
 
             // 3초 후 상태를 'in'으로 변경
             setTimeout(() => {
-              updateParticipant(studentId, { status: 'in' });
+              updateParticipant(studentId, {status: 'in'});
             }, 3000);
           } else if (type === 'out') {
-            updateParticipant(studentId, { status: 'out' });
+            updateParticipant(studentId, {status: 'out'});
             setTimeout(() => {
               removeParticipant(studentId);
             }, 3000);
@@ -123,7 +159,7 @@ function LessoningStudentListScreen(): React.JSX.Element {
       onDisconnect: () => {
         console.log('STOMP client disconnected');
       },
-      onWebSocketError: (error) => {
+      onWebSocketError: error => {
         console.error('WebSocket Error:', error);
       },
     });
@@ -137,7 +173,7 @@ function LessoningStudentListScreen(): React.JSX.Element {
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [addParticipant, removeParticipant, updateParticipant, participants]);
 
   return (
     <View style={styles.mainContainer}>
@@ -159,10 +195,10 @@ function LessoningStudentListScreen(): React.JSX.Element {
       ) : (
         <FlatList
           data={currentParticipants}
-          keyExtractor={(item) => String(item.studentId)}
+          keyExtractor={item => String(item.studentId)}
           numColumns={COLUMNS}
           contentContainerStyle={styles.grid}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <ParticipantCard
               participant={item}
               onPress={() => handleParticipantPress(item.studentId)}
@@ -203,7 +239,6 @@ function LessoningStudentListScreen(): React.JSX.Element {
 }
 
 export default LessoningStudentListScreen;
-
 
 const styles = StyleSheet.create({
   mainContainer: {
