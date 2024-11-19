98e20dda506ee87161c45b4f50a7ecce967c8660
이지언 <dlwldjs7544@naver.com>
2024-11-19 11:14:07 +0900

    Revert "backend/develop branch -> Main branch 병합"
    This reverts merge request !258

--- backend/lecture-service/src/main/java/com/eum/lecture_service/command/repository/homework/HomeworkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eum.lecture_service.command.repository.homework;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eum.lecture_service.command.entity.homework.Homework;
-
-public interface HomeworkRepository extends JpaRepository<Homework, Long> {
-}
--- backend/lecture-service/src/main/java/com/eum/lecture_service/command/repository/notice/NoticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eum.lecture_service.command.repository.notice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eum.lecture_service.command.entity.lecture.Notice;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
